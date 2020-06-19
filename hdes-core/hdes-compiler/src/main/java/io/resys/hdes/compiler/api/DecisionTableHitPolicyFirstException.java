7fac50c671d27633bdeedff49706c8e138b84e36
Olev Mutso <olev.mutso@resys.io>
2020-06-19 09:37:25 +0300

    hit policy first compiler

---
+++ hdes-core/hdes-compiler/src/main/java/io/resys/hdes/compiler/api/DecisionTableHitPolicyFirstException.java
@@ -0,0 +1,11 @@
+package io.resys.hdes.compiler.api;
+
+public class DecisionTableHitPolicyFirstException extends RuntimeException {
+  private static final long serialVersionUID = -2741344591344941378L;
+
+  public DecisionTableHitPolicyFirstException(String message) {
+    super(message);
+  }
+  
+  
+}
