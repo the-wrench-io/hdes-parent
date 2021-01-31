be80e4c0d4e168acc22f280bf5cef73750043f88
Olev Mutso <olev.mutso@resys.io>
2021-01-31 12:36:53 +0200

    cleanup

---
+++ hdes-dev-tools/hdes-pm-frontend/src/core/Resources/Backend/Server/ServerGroupService.tsx
@@ -0,0 +1,59 @@
+import Backend from './../Backend';
+import { GenericGroupBuilder } from './../Builders';
+import { Store } from './ServerStore';
+
+
+class ServerUserQuery implements Backend.GroupQuery {
+  store: Store;
+  args?: {top?: number};
+  
+  constructor(store: Store, args?: {top?: number}) {
+    this.store = store;
+    this.args = args;
+  }
+  
+  onSuccess(handle: (users: Backend.GroupResource[]) => void) {
+    const store = this.store;
+    const init: RequestInit = { method: 'GET', credentials: 'same-origin', headers: store.config.headers }
+    const url: string = store.config.users
+    store.fetch<Backend.GroupResource[]>(url, init).then(handle)
+  }
+}
+
+class ServerUserService implements Backend.GroupService {
+  store: Store;
+  constructor(store: Store) {
+    this.store = store;
+  }
+  
+  query(args?: {top?: number}) {
+    return new ServerUserQuery(this.store, args);
+  }
+  builder(from?: Backend.GroupResource) {
+    const result = new GenericGroupBuilder();
+    if(from) {
+      return result.withResource(from);
+    }
+    return result;
+  }
+  save(builder: Backend.GroupBuilder) {
+    const store = this.store;
+    return {
+      onSuccess: (callback: (resource: Backend.GroupResource) => void) => {        
+        const store = this.store;
+        const init: RequestInit = { 
+          method: 'POST', 
+          credentials: 'same-origin', 
+          headers: store.config.headers,
+          body: JSON.stringify(builder.build())
+        };
+          
+        const url: string = store.config.users
+        store.fetch<Backend.GroupResource>(url, init)
+          .then(callback)
+      }
+    }
+  }
+}
+
+export { ServerUserService };
---
+++ hdes-dev-tools/hdes-pm-frontend/src/core/Resources/Backend/Server/ServerProjectService.tsx
@@ -0,0 +1,59 @@
+import Backend from './../Backend';
+import { GenericProjectBuilder } from './../Builders';
+import { Store } from './ServerStore';
+
+
+class ServerUserQuery implements Backend.ProjectQuery {
+  store: Store;
+  args?: {top?: number};
+  
+  constructor(store: Store, args?: {top?: number}) {
+    this.store = store;
+    this.args = args;
+  }
+  
+  onSuccess(handle: (users: Backend.ProjectResource[]) => void) {
+    const store = this.store;
+    const init: RequestInit = { method: 'GET', credentials: 'same-origin', headers: store.config.headers }
+    const url: string = store.config.users
+    store.fetch<Backend.ProjectResource[]>(url, init).then(handle)
+  }
+}
+
+class ServerUserService implements Backend.ProjectService {
+  store: Store;
+  constructor(store: Store) {
+    this.store = store;
+  }
+  
+  query(args?: {top?: number}) {
+    return new ServerUserQuery(this.store, args);
+  }
+  builder(from?: Backend.ProjectResource) {
+    const result = new GenericProjectBuilder();
+    if(from) {
+      return result.withResource(from);
+    }
+    return result;
+  }
+  save(builder: Backend.ProjectBuilder) {
+    const store = this.store;
+    return {
+      onSuccess: (callback: (resource: Backend.ProjectResource) => void) => {        
+        const store = this.store;
+        const init: RequestInit = { 
+          method: 'POST', 
+          credentials: 'same-origin', 
+          headers: store.config.headers,
+          body: JSON.stringify(builder.build())
+        };
+          
+        const url: string = store.config.users
+        store.fetch<Backend.ProjectResource>(url, init)
+          .then(callback)
+      }
+    }
+  }
+}
+
+export { ServerUserService };
