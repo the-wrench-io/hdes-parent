40fa1d5340986d15471fec72f75595353497b805
Olev Mutso <olev.mutso@resys.io>
2021-02-19 13:44:02 +0200

    resource editor

--- hdes-dev-tools/hdes-re-frontend/src/App.tsx
+++ hdes-dev-tools/hdes-re-frontend/src/App.tsx
@@ -6,10 +6,12 @@ import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
 import AccountTreeIcon from '@material-ui/icons/AccountTree';
 import CallMergeIcon from '@material-ui/icons/CallMerge';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import { Resources, Backend, Session } from './core/Resources';
 import ViewQuiltIcon from '@material-ui/icons/ViewQuilt';
 
-import { Tabs, TabPanel } from './core/Tabs';
+import { Resources, Backend, Session } from './core/Resources';
+import { Tabs, TabPanelRenderer } from './core/Tabs';
+import { AssetsView } from './core/Assets';
+import { ProjectsView } from './core/Projects';
 import Shell from './core/Shell';
 
 
@@ -28,25 +30,31 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
 
-  
   const { session, setSession } = React.useContext(Resources.Context);
-
   const handleSearch = (keyword: string) => setSession((session) => session.setSearch(keyword))
-
-
+  const handleAddTab = (newItem: Session.Tab<any>) => setSession((session) => session.addTab(newItem));
+  
+  const listProjects = () => handleAddTab({id: 'static/projects', label: 'Projects' });
+  
   const views = [
-    { id: 'resources', label: 'View Resources', icon: <LibraryBooksIcon />, onClick: () => console.log("add resource") },
-    { id: 'add-resource', label: 'Add Resource', icon: <LibraryAddIcon />, onClick: () => console.log("add resource") },
+    { id: 'assets', label: 'View Assets', icon: <LibraryBooksIcon />, onClick: () => console.log("add resource") },
+    { id: 'add-asset', label: 'Add Asset', icon: <LibraryAddIcon />, onClick: () => console.log("add resource") },
     { id: 'branchs', label: 'Set Branch', icon: <AccountTreeIcon />, onClick: () => console.log("set branch") },
     { id: 'merge', label: 'Merge To Main', icon: <CallMergeIcon />, onClick: () => console.log("Merge") },
-    { id: 'projects', label: 'View Projects', icon: <ViewQuiltIcon />, onClick: () => console.log("add resource") },
+    { id: 'projects', label: 'View Projects', icon: <ViewQuiltIcon />, onClick: () => listProjects() },
   ];
   
   const view = (<div>sdfjksdfkhsdkfhsdhfksdhfksdhkfjh</div>)
   
+  
+  const tabPanel = <TabPanelRenderer plugins={[
+    { id: 'static/projects', view: <ProjectsView /> },
+    { view: <AssetsView />}
+  ]}/>
+  
   return (<React.Fragment>
     <Shell 
-      tabs={{items: <Tabs />, panel: <></>}}
+      tabs={{items: <Tabs />, panel: tabPanel}}
       views={views}
       search={{ onChange: handleSearch }}
       badges={[  ]}>
--- hdes-dev-tools/hdes-re-frontend/src/core/Tabs/index.tsx
+++ hdes-dev-tools/hdes-re-frontend/src/core/Tabs/index.tsx
@@ -1,4 +1,5 @@
 import TabsView from './TabsView';
 import TabPanel from './TabPanel';
+import TabPanelRenderer from './TabPanelRenderer'
 
-export { TabsView as Tabs, TabPanel };
+export { TabsView as Tabs, TabPanel, TabPanelRenderer };
