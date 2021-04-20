b5804113bacea77ebe03985c9c8156446f0c8a51
Olev Mutso <olev.mutso@resys.io>
2021-04-20 19:41:16 +0300

    tag functionality

---
+++ hdes-dev-tools/hdes-docdb/src/test/java/io/resys/hdes/docdb/tests/TaggingTest.java
@@ -0,0 +1,91 @@
+package io.resys.hdes.docdb.tests;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.resys.hdes.docdb.api.actions.CommitActions.CommitResult;
+import io.resys.hdes.docdb.api.actions.CommitActions.CommitStatus;
+import io.resys.hdes.docdb.api.actions.RepoActions.RepoResult;
+import io.resys.hdes.docdb.api.actions.RepoActions.RepoStatus;
+import io.resys.hdes.docdb.api.actions.TagActions.TagResult;
+import io.resys.hdes.docdb.api.actions.TagActions.TagStatus;
+import io.resys.hdes.docdb.tests.config.MongoDbConfig;
+
+
+public class TaggingTest extends MongoDbConfig {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TaggingTest.class);
+  
+  
+  @Test
+  public void crateRepoWithTwoCommitsAndTagBothCommits() {
+    // create project
+    RepoResult repo = getClient().repo().create()
+        .name("project-xyz")
+        .build()
+        .await().atMost(Duration.ofMinutes(1));
+    LOGGER.debug("created repo {}", repo);
+    Assertions.assertEquals(RepoStatus.OK, repo.getStatus());
+    
+    // Create head and first commit
+    CommitResult commit_0 = getClient().commit().head()
+      .head(repo.getRepo().getName(), "main")
+      .append("readme.md", "readme content")
+      .append("file1.json", "[{}]")
+      .author("same vimes")
+      .message("first commit!")
+      .build()
+      .await().atMost(Duration.ofMinutes(1));
+
+    LOGGER.debug("created commit 0 {}", commit_0);
+    Assertions.assertEquals(CommitStatus.OK, commit_0.getStatus());
+    
+    
+    // Create head and first commit
+    CommitResult commit_1 = getClient().commit().head()
+      .head(repo.getRepo().getName(), "main")
+      .parent(commit_0.getCommit().getId())
+      .append("readme.md", "readme content")
+      .append("file1.json", "[{}, {}]")
+      .author("same vimes")
+      .message("second commit!")
+      .build()
+      .await().atMost(Duration.ofMinutes(1));
+    
+    LOGGER.debug("created commit 1 {}", commit_1);
+    Assertions.assertEquals(CommitStatus.OK, commit_1.getStatus());
+    
+    
+    TagResult tag_0 = getClient().tag().create()
+      .tagName("super tag on commit 1")
+      .author("same vimes")
+      .message("first commit tag")
+      .repo(repo.getRepo().getName(), commit_0.getCommit().getId())
+      .build()
+      .await().atMost(Duration.ofMinutes(1));
+    Assertions.assertEquals(TagStatus.OK, tag_0.getStatus());
+
+    TagResult tag_1 = getClient().tag().create()
+        .tagName("Tagging again")
+        .author("same vimes")
+        .message("second commit tag")
+        .repo(repo.getRepo().getName(), commit_1.getCommit().getId())
+        .build()
+        .await().atMost(Duration.ofMinutes(1));
+    Assertions.assertEquals(TagStatus.OK, tag_1.getStatus());
+    
+    List<String> tagNames = getClient().tag().query().repo(repo.getRepo().getName()).find().collectItems().asList()
+    .await().indefinitely().stream().map(t -> t.getName()).collect(Collectors.toList());
+    
+    Assertions.assertTrue(tagNames.contains(tag_0.getTag().getName()));
+    Assertions.assertTrue(tagNames.contains(tag_1.getTag().getName()));
+    
+    super.printRepo(repo.getRepo());
+  }
+}
