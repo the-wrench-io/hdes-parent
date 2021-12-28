e60f13bc81da0acf460c74f9c672a57e5e45f40a
Olev Mutso <olev.mutso@resys.io>
2021-12-28 09:47:51 +0200

    release test

--- hdes-build-parent/release.sh
+++ hdes-build-parent/release.sh
@@ -21,19 +21,6 @@ echo "Setup git user name to '$BOT_NAME' and email to '$BOT_EMAIL' GPG key ID $G
 git config --global user.name "$BOT_NAME";
 git config --global user.email "$BOT_EMAIL";
 
-# Checkout
-git reset --hard
-git fetch --all
-git branch -a --contains ${GITHUB_SHA} --format="%(refname)"
-
-readonly local refname=$(git branch -a --contains ${GITHUB_SHA} --format="%(refname)" | head -1)
-if [[ "${refname}" = "refs/heads/3.y" ]]; then
-     readonly local branch="3.y"
-else
-     readonly local branch=${refname#refs/remotes/origin/}
-fi
-
-
 # Current and next version
 LAST_RELEASE_VERSION=$(cat hdes-build-parent/release.version)
 [[ $LAST_RELEASE_VERSION =~ ([^\\.]*)$ ]]
@@ -42,15 +29,12 @@ MAJOR_VERSION=${LAST_RELEASE_VERSION:0:`expr ${#LAST_RELEASE_VERSION} - ${#MINOR
 RELEASE_VERSION=${MAJOR_VERSION}${MINOR_VERSION}
 echo ${RELEASE_VERSION} > hdes-build-parent/release.version
 
-
 PROJECT_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 echo "Git checkout refname: '${refname}' branch: '${branch}' commit: '${GITHUB_SHA}'"
 echo "Dev version: '${PROJECT_VERSION}' release version: '${RELEASE_VERSION}'"
 echo "Releasing: '${RELEASE_VERSION}', previous: '${LAST_RELEASE_VERSION}'"
-mvn -version
 
-git checkout ${branch}
 mvn versions:set -DnewVersion=${RELEASE_VERSION}
 git commit -am "Release: ${RELEASE_VERSION}"
 git tag -a ${RELEASE_VERSION} -m "release ${RELEASE_VERSION}"
