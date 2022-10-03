8ace3d3677506ac8466bc8511a4385d44b50530d
Olev Mutso <olev.mutso@resys.io>
2022-10-03 13:54:13 +0300

    file store

---
+++ hdes-core/hdes-client-api/src/main/java/io/resys/hdes/client/spi/HdesStoreFileImpl.java
@@ -0,0 +1,174 @@
+package io.resys.hdes.client.spi;
+
+/*-
+ * #%L
+ * hdes-client-api
+ * %%
+ * Copyright (C) 2020 - 2022 Copyright 2020 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import groovy.util.logging.Slf4j;
+import io.resys.hdes.client.api.HdesStore;
+import io.resys.hdes.client.api.ImmutableStoreEntity;
+import io.resys.hdes.client.spi.store.BlobDeserializer;
+import io.resys.hdes.client.spi.store.ImmutableThenaConfig;
+import io.resys.hdes.client.spi.store.ThenaConfig;
+import io.resys.hdes.client.spi.store.ThenaConfig.AuthorProvider;
+import io.resys.hdes.client.spi.store.ThenaConfig.GidProvider;
+import io.resys.hdes.client.spi.store.ThenaStoreTemplate;
+import io.resys.hdes.client.spi.util.HdesAssert;
+import io.resys.thena.docdb.api.DocDB;
+import io.resys.thena.docdb.file.DocDBFactoryFile;
+import io.resys.thena.docdb.file.FileErrors;
+import io.resys.thena.docdb.file.spi.FilePoolImpl;
+import io.resys.thena.docdb.file.tables.Table.FilePool;
+
+public class HdesStoreFileImpl extends ThenaStoreTemplate implements HdesStore {
+  private static final Logger LOGGER = LoggerFactory.getLogger(HdesStoreFileImpl.class);
+
+  public HdesStoreFileImpl(ThenaConfig config) {
+    super(config);
+  }
+
+  @Override
+  protected HdesStoreFileImpl createWithNewConfig(ThenaConfig config) {
+    return new HdesStoreFileImpl(config);
+  }
+  
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  @Slf4j
+  public static class Builder {
+    private String repoName;
+    private String headName;
+    private ObjectMapper objectMapper;
+    private GidProvider gidProvider;
+    private AuthorProvider authorProvider;
+    private String db;
+    private FilePool pool;
+    
+    public Builder repoName(String repoName) {
+      this.repoName = repoName;
+      return this;
+    }
+    public Builder objectMapper(ObjectMapper objectMapper) {
+      this.objectMapper = objectMapper;
+      return this;
+    }
+    public Builder gidProvider(GidProvider gidProvider) {
+      this.gidProvider = gidProvider;
+      return this;
+    }
+    public Builder authorProvider(AuthorProvider authorProvider) {
+      this.authorProvider = authorProvider;
+      return this;
+    }
+    public Builder pgPool(FilePool pgPool) {
+      this.pool = pgPool;
+      return this;
+    }
+    public Builder headName(String headName) {
+      this.headName = headName;
+      return this;
+    }
+    public Builder pgDb(String pgDb) {
+      this.db = pgDb;
+      return this;
+    }
+    
+    private GidProvider getGidProvider() {
+      return this.gidProvider == null ? type -> {
+        return UUID.randomUUID().toString();
+     } : this.gidProvider;
+    }
+    
+    private AuthorProvider getAuthorProvider() {
+      return this.authorProvider == null ? ()-> "not-configured" : this.authorProvider;
+    } 
+    
+    private ObjectMapper getObjectMapper() {
+      if(this.objectMapper == null) {
+        return this.objectMapper;
+      }
+      
+      final ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.registerModule(new GuavaModule());
+      objectMapper.registerModule(new JavaTimeModule());
+      objectMapper.registerModule(new Jdk8Module());
+      return objectMapper;
+    }
+    
+    public HdesStoreFileImpl build() {
+      HdesAssert.notNull(repoName, () -> "repoName must be defined!");
+    
+      final var headName = this.headName == null ? "main": this.headName;
+      if(LOGGER.isDebugEnabled()) {
+        final var log = new StringBuilder()
+          .append(System.lineSeparator())
+          .append("Configuring Thena: ").append(System.lineSeparator())
+          .append("  repoName: '").append(this.repoName).append("'").append(System.lineSeparator())
+          .append("  headName: '").append(headName).append("'").append(System.lineSeparator())
+          .append("  objectMapper: '").append(this.objectMapper == null ? "configuring" : "provided").append("'").append(System.lineSeparator())
+          .append("  gidProvider: '").append(this.gidProvider == null ? "configuring" : "provided").append("'").append(System.lineSeparator())
+          .append("  authorProvider: '").append(this.authorProvider == null ? "configuring" : "provided").append("'").append(System.lineSeparator())
+          .append("  db: '").append(this.db).append("'").append(System.lineSeparator());
+          
+        LOGGER.debug(log.toString());
+      }
+      
+      final DocDB thena;
+      if(pool == null) {
+        HdesAssert.notNull(db, () -> "db must be defined!");
+        final var pgPool = new FilePoolImpl(new File(db), objectMapper);
+        
+        thena = DocDBFactoryFile.create().client(pgPool).db(repoName).errorHandler(new FileErrors()).build();
+      } else {
+        thena = DocDBFactoryFile.create().client(pool).db(repoName).errorHandler(new FileErrors()).build();
+      }
+      
+      final ObjectMapper objectMapper = getObjectMapper();
+      final ImmutableThenaConfig config = ImmutableThenaConfig.builder()
+          .client(thena).repoName(repoName).headName(headName)
+          .gidProvider(getGidProvider())
+          .serializer((entity) -> {
+            try {
+              return objectMapper.writeValueAsString(ImmutableStoreEntity.builder().from(entity).build());
+            } catch (IOException e) {
+              throw new RuntimeException(e.getMessage(), e);
+            }
+          })
+          .deserializer(new BlobDeserializer(objectMapper))
+          .authorProvider(getAuthorProvider())
+          .build();
+      return new HdesStoreFileImpl(config);
+    }
+  }
+  
+}
---
+++ hdes-core/hdes-client-api/src/main/java/io/resys/hdes/client/spi/store/ThenaStoreTemplate.java
@@ -0,0 +1,210 @@
+package io.resys.hdes.client.spi.store;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*-
+ * #%L
+ * hdes-client-api
+ * %%
+ * Copyright (C) 2020 - 2021 Copyright 2020 ReSys OÜ
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.resys.hdes.client.api.HdesStore;
+import io.resys.hdes.client.api.ImmutableStoreEntity;
+import io.resys.hdes.client.api.ImmutableStoreExceptionMsg;
+import io.resys.hdes.client.api.ast.AstBody.AstBodyType;
+import io.resys.hdes.client.api.exceptions.StoreException;
+import io.resys.hdes.client.spi.store.ThenaConfig.EntityState;
+import io.resys.hdes.client.spi.util.HdesAssert;
+import io.resys.thena.docdb.api.actions.CommitActions.CommitStatus;
+import io.resys.thena.docdb.api.actions.ObjectsActions.ObjectsStatus;
+import io.resys.thena.docdb.api.actions.RepoActions.RepoStatus;
+import io.smallrye.mutiny.Uni;
+
+public abstract class ThenaStoreTemplate extends PersistenceCommands implements HdesStore {
+  public ThenaStoreTemplate(ThenaConfig config) {
+    super(config);
+  }
+  
+  protected abstract HdesStore createWithNewConfig(ThenaConfig config);
+  
+  @Override
+  public String getRepoName() {
+    return config.getRepoName();
+  }
+  @Override
+  public String getHeadName() {
+    return config.getHeadName();
+  }
+  @Override
+  public StoreRepoBuilder repo() {
+    return new StoreRepoBuilder() {
+      private String repoName;
+      private String headName;
+      @Override
+      public StoreRepoBuilder repoName(String repoName) {
+        this.repoName = repoName;
+        return this;
+      }
+      @Override
+      public StoreRepoBuilder headName(String headName) {
+        this.headName = headName;
+        return this;
+      }
+      @Override
+      public Uni<HdesStore> create() {
+        HdesAssert.notNull(repoName, () -> "repoName must be defined!");
+        final var client = config.getClient();
+        final var newRepo = client.repo().create().name(repoName).build();
+        return newRepo.onItem().transform((repoResult) -> {
+          if(repoResult.getStatus() != RepoStatus.OK) {
+            throw new StoreException("REPO_CREATE_FAIL", null, 
+                ImmutableStoreExceptionMsg.builder()
+                .id(repoResult.getStatus().toString())
+                .value(repoName)
+                .addAllArgs(repoResult.getMessages().stream().map(message->message.getText()).collect(Collectors.toList()))
+                .build()); 
+          }
+          
+          return build();
+        });
+      }
+      @Override
+      public HdesStore build() {
+        HdesAssert.notNull(repoName, () -> "repoName must be defined!");
+        return createWithNewConfig(ImmutableThenaConfig.builder()
+            .from(config)
+            .repoName(repoName)
+            .headName(headName == null ? config.getHeadName() : headName)
+            .build());
+      }
+      @Override
+      public Uni<Boolean> createIfNot() {
+        final var client = config.getClient();
+        
+        return client.repo().query().id(config.getRepoName()).get().onItem().transformToUni(repo -> {
+          if(repo == null) {
+            return client.repo().create().name(config.getRepoName()).build().onItem().transform(newRepo -> true); 
+          }
+          return Uni.createFrom().item(true);
+        });
+      }
+    };
+  }
+  
+  @Override
+  public QueryBuilder query() {
+    return new DocumentQueryBuilder(config);
+  }
+  @Override
+  public Uni<StoreEntity> create(CreateStoreEntity newType) {
+    final var gid = gid(newType.getBodyType());
+    final var entity = (StoreEntity) ImmutableStoreEntity.builder()
+        .id(gid)
+        .hash("")
+        .body(newType.getBody())
+        .bodyType(newType.getBodyType())
+        .build();
+    return super.save(entity);
+  }
+
+  @Override
+  public Uni<StoreEntity> update(UpdateStoreEntity updateType) {
+    final Uni<EntityState> query = getEntityState(updateType.getId());
+    return query.onItem().transformToUni(state -> {
+      final StoreEntity entity = ImmutableStoreEntity.builder()
+          .from(state.getEntity())
+          .id(updateType.getId())
+          .bodyType(state.getEntity().getBodyType())
+          .body(updateType.getBody())
+          .build();
+      return super.save(entity);
+    });
+  }
+  @Override
+  public Uni<List<StoreEntity>> batch(ImportStoreEntity batchType) {
+    final var commitBuilder = config.getClient().commit().head()
+        .head(config.getRepoName(), config.getHeadName())
+        .message("Save batch with new: " + batchType.getCreate().size() + " and updated: " + batchType.getUpdate().size() + " entries")
+        .parentIsLatest()
+        .author(config.getAuthorProvider().getAuthor());
+    
+    final List<String> ids = new ArrayList<>();
+    for(final var toBeSaved : batchType.getCreate()) {
+      final var gid = gid(toBeSaved.getBodyType());
+      final var entity = (StoreEntity) ImmutableStoreEntity.builder()
+          .id(gid)
+          .hash("")
+          .body(toBeSaved.getBody())
+          .bodyType(toBeSaved.getBodyType())
+          .build();
+      commitBuilder.append(entity.getId(), config.getSerializer().toString(entity));
+      ids.add(gid);
+    }
+    for(final var toBeSaved : batchType.getUpdate()) {
+      final var entity = (StoreEntity) ImmutableStoreEntity.builder()
+          .id(toBeSaved.getId())
+          .hash("")
+          .body(toBeSaved.getBody())
+          .bodyType(toBeSaved.getBodyType())
+          .build();
+      commitBuilder.append(entity.getId(), config.getSerializer().toString(entity));
+      ids.add(entity.getId());
+    }    
+    
+    return commitBuilder.build().onItem().transformToUni(commit -> {
+          if(commit.getStatus() == CommitStatus.OK) {
+            return config.getClient()
+                .objects().blobState()
+                .repo(config.getRepoName())
+                .anyId(config.getHeadName())
+                .blobNames(ids)
+                .list().onItem()
+                .transform(states -> {
+                  if(states.getStatus() != ObjectsStatus.OK) {
+                    // TODO
+                    throw new StoreException("LIST_FAIL", null, convertMessages2(states));
+                  }
+                  List<StoreEntity> entities = new ArrayList<>(); 
+                  for(final var state : states.getObjects().getBlob()) {
+                    StoreEntity start = (StoreEntity) config.getDeserializer().fromString(state);
+                    entities.add(start);
+                  }                  
+                  return entities;
+                });
+          }
+          // TODO
+          throw new StoreException("SAVE_FAIL", null, convertMessages(commit));
+        });
+  }
+  @Override
+  public Uni<StoreEntity> delete(DeleteAstType deleteType) {
+    final Uni<EntityState> query = getEntityState(deleteType.getId());
+    return query.onItem().transformToUni(state -> delete(state.getEntity()));
+  }
+
+  private String gid(AstBodyType type) {
+    return config.getGidProvider().getNextId(type);
+  }
+
+  @Override
+  public HistoryQuery history() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
