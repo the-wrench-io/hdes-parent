c3e316ecdee454e31c356f0b49d8e2698d75a0ce
admirkrilasevic <admirkrilasevic@gmail.com>
2024-04-25 14:15:02 +0200

    added test for hardcoded flow step input

--- hdes-spring/hdes-spring-composer/src/test/java/io/resys/wrench/assets/bundle/spi/FlowTaskServiceExecutionTest.java
+++ hdes-spring/hdes-spring-composer/src/test/java/io/resys/wrench/assets/bundle/spi/FlowTaskServiceExecutionTest.java
@@ -103,6 +103,12 @@ public class FlowTaskServiceExecutionTest {
   }
 
   @Test
+  public void testHardcodedFlowInput() {
+    final var body = client.executor(envir).inputField("myInputParam", "").flow("hardcodedInput").andGetBody();
+    Assert.assertEquals("test", body.getReturns().get("res"));
+  }
+
+  @Test
   public void flowCsvDebugSingle() {
     ImmutableDebugRequest entity = ImmutableDebugRequest.builder()
             .id("sumFlow.json")
